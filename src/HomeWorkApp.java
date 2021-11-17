276dfce3924ec641f44b907277ca5fd01381c915
Macbook Pro <macbookpro@MacBook-Pro-Macbook-2.local>
2021-11-17 13:55:53 +0300

    Initial commit

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,49 @@
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+
+    }
+        public static void main(){}
+        public static void printThreeWorlds(){
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+    }
+        public static void checkSumSign(){
+            int a = 5;
+            int b = 10;
+            int c = a + b;
+                if (c >= 0 ) {
+                    System.out.println("Сумма положительная");
+        }
+                else {
+                    System.out.println("Сумма отрицаьельная");
+            }
+        }
+        public static void printColor(){
+            int value = 37;
+                if (value <= 0 ){
+                    System.out.println("Красный");
+        }
+                    else if (value > 0 && value <=100) {
+                        System.out.println("Желтый");
+            }
+                        else {
+                            System.out.println("Зеленый");
+        }
+        }
+        public static void compareNumber(){
+            int a = 25;
+            int b = 49;
+                if(a >= b) {
+                    System.out.println("a>=b");
+                }
+                    else {
+                       System.out.println("a<b");
+                    }
+
+        }
+
+
+}
+
